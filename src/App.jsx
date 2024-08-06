67a2f15f48f67ba98f05bc21e4e2ffc3625ecba5
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-06 19:56:45 +0200

    User interfaces
    This code is a part of a React application, used for creating user interfaces in JavaScript.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactDOM from "react-dom/client";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,4 +10,8 @@ function App() {
   return <></>
 }
 
-export default App
+const rootElement = document.getElementById('root');
+const root  = ReactDOM.createRoot(rootElement);
+root.render(<App/>);
+
+export default App;
