0c48df308650539df20377ef8fff896c46a66024
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-08 10:37:26 +0200

    Navigations
    A React application using the React Router library for navagitaion  management
    Germany:
    Eine React-Anwendung, die die React Router-Bibliothek zur Navigationsverwaltung verwendet
    Japan:
    ナビゲーション管理のためにReact Routerライブラリを使用するReactアプリケーション。

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,41 @@ import { useState } from 'react'
 import {BrowserRouter,Outlet , Route ,Routes} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Header} from './jsx/Header.jsx'
+import {Contact} from './jsx/Contact.jsx'
+import {HelpCenter} from './jsx/HelpCenter.jsx'
+import {Products} from './jsx/Products.jsx'
 import './App.css'
 import ReactDOM from "react-dom/client";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return <></>
+const Layout = () =>{
+  return(
+      <div className= 'App'>
+        <Header/>
+        <Outlet/>
+      </div>
+  )
+}
+
+const Router = () =>{
+  return (
+      <BrowserRouter>
+        <Routes path ='/' element={<Layout/>}>
+          <Routes path ='/contact' element = {<Contact/>} />
+          <Routes path ='/HelpCenter' element = {<HelpCenter/>} />
+          <Routes path ='/Products' element = {<Products/>} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+const App = () => {
+
+  return (
+     <>
+     <Router/>
+     </>
+  );
 }
 
 const rootElement = document.getElementById('root');
