4c80235d6d039b7f54405f13ca9f20f4c40de62d
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-09 08:18:53 +0200

    HeaderSection
    Creating a Header component in React, which represents the header of a web page. It is built using a function and utilizes JSX, an extension of JavaScript syntax for describing UI elements. Below, I describe the code details:
    
    Germany:
    Erstellen einer Header-Komponente in React, die den Kopfbereich einer Webseite darstellt. Sie wird mithilfe einer Funktion erstellt und verwendet JSX, eine Erweiterung der JavaScript-Syntax zur Beschreibung von UI-Elementen. Unten beschreibe ich die Details des Codes:
    
    Japan:
    Reactでウェブページのヘッダーを表すHeaderコンポーネントを作成します。これは関数を使用して作成され、UI要素を記述するためのJavaScript構文拡張であるJSXを利用しています。以下に、コードの詳細を説明します。

---
+++ src/jsx/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import '../scss/_main.scss';
+import {Link} from "react-router-dom";
+
+export const Header = () =>{
+    return(
+        <>
+        <header className='header'>
+            <div className='header__container container'>
+                <a href='#' className='logo'>DG</a>
+                <nav>
+                    <input type='checkbox' className= "menu__btn" id='menu__btn'/>
+                    <label htmlFor='menu__btn' className='menu__toggle'>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                    <ul className='header__nav'>
+                        <li className='nav__element'>
+                            <Link to='/' className='nav__link'>Strona główna </Link>
+                        </li>
+                        <li className='nav__element'>
+                            <Link to='/Products' className='nav__link'>Produkty</Link>
+                        </li>
+                        <li className='nav__element'>
+                            <Link to='/HelpCenter' className='nav__link'>Centrum pomocy </Link>
+                        </li>
+                        <li className='nav__element'>
+                            <Link to='/Contact' className='nav__link'>Kontakt </Link>
+                        </li>
+
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+        </>
+    )
+};
