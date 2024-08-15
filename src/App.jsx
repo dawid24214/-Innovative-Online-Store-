f5e5967adb7d6231aa150aae2162cfa0241c5f6a
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-15 10:55:19 +0200

    The corrections from the API side

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
+import React from "react";
 import {BrowserRouter,Outlet , Route ,Routes} from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Header} from './jsx/Header.jsx'
 import {Contact} from './jsx/Contact.jsx'
 import {HelpCenter} from './jsx/HelpCenter.jsx'
@@ -20,15 +18,17 @@ const Layout = () =>{
 }
 
 const Router = () =>{
-  return (
-      <BrowserRouter>
-        <Routes path ='/' element={<Layout/>}>
-          <Routes path ='/contact' element = {<Contact/>} />
-          <Routes path ='/HelpCenter' element = {<HelpCenter/>} />
-          <Routes path ='/Products' element = {<Products/>} />
-        </Routes>
-      </BrowserRouter>
-  )
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout/>}>
+                    <Route path='/contact' element={<Contact/>} />
+                    <Route path='/HelpCenter' element={<HelpCenter/>} />
+                    <Route path='/Products' element={<Products/>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 const App = () => {
 
