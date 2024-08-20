c4f87b9066d84ff518257938cdd4176418d47701
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-20 15:18:22 +0200

    Contact Table Description:
    I have created a contact table that allows users to conveniently send messages to our store. With this table, users can easily get in touch with us, ask questions, provide feedback, or share suggestions.

--- src/jsx/Contact.jsx
+++ src/jsx/Contact.jsx
@@ -1,7 +1,32 @@
+import '../scss/elements/_contact.scss';
+
 export const Contact = () =>{
     return(
-        <div>
-        strona kontaktowa
-    </div>
+        <section className='container__section'>
+            <form>
+                <h2>Napisz do nas !</h2>
+                <div className='input-box'>
+                    <label>Imie i Nazwisko*</label>
+                    <input type='text' className='field' placeholder='Podaj swoje imie' required/>
+                </div>
+                <div className='input-box'>
+                    <label>Email*</label>
+                    <input type='text' className='field' placeholder='Podaj swoj Email' required/>
+                </div>
+                <div className='input-box'>
+                    <label>Temat*</label>
+                    <input type='text' className='field' placeholder='Podaj Temat' required/>
+                </div>
+                <div className='input-box'>
+                    <label>Telefon</label>
+                    <input type='text' className='field' placeholder='Podaj swoj numer ' required/>
+                </div>
+                <div className='input-box '>
+                    <label>Treść wiadomosci*</label>
+                    <input type='text' className='field array__text' placeholder='' required/>
+                </div>
+            </form>
+
+        </section>
     )
 }
