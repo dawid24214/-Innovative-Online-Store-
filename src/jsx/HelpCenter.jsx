7d8f1b0a8ced70ff227e20e2690800e5001590b3
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-08-21 07:03:48 +0200

    Solving the problem with adding a photo

--- src/jsx/HelpCenter.jsx
+++ src/jsx/HelpCenter.jsx
@@ -1,7 +1,18 @@
-export const  HelpCenter = () => {
+import '../scss/elements/_helpcenter.scss';
+
+export const Image = () =>{
+    <img
+    src='../assets/Blue%20and%20Purple%20Social%20Media%20Specialist%20LinkedIn%20Article%20Cover%20Image%20(2).png'
+    height='175'
+    width='200'
+    ></img>
+};
+
+export const HelpCenter = () => {
     return(
-        <div>
-            <div>Centrum pomocy</div>
-        </div>
-    )
-}
+        <>
+        <Image/>
+        </>
+    );
+};
+
